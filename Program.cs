91b2865f00b1a763752dedf83b901f0f79706d99
Lasse Hartmann <Lasse.BlueBird@web.de>
2021-12-02 09:09:45 +0100

    Day1 finished
    - Struct started
    - Day1 finished

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using aoc.structure;
+using aoc21.day1;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace aoc.twentyOne
+{
+
+    public class Program
+    {
+
+        public static List<Day> Days = new List<Day>()
+        {
+            new Day1()
+        };
+
+        public static void Main(string[] args)
+        {
+            if(args.Length >= 1 && args.Length <= 2)
+            {
+                int dayNumber = -1;
+                if(int.TryParse(args[0], out dayNumber))
+                {
+                    Day day = Days[dayNumber - 1];
+
+                    string path = args.Length >= 2 ? args[1] : day.DefaultPath;
+
+                    Stopwatch stopwatch = new Stopwatch();
+                    stopwatch.Start();
+
+                    string computeResult = day.ComputeDay(path);
+
+                    stopwatch.Stop();
+
+                    if(computeResult == null)
+                    {
+                        Console.WriteLine($"Compute of Day {dayNumber} with InputeFile {path} has resulted in internal ComputeError");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Day {dayNumber} finished in {stopwatch.ElapsedMilliseconds}ms:\n{computeResult}");
+                    }
+                    return;
+                }
+                Console.WriteLine($"The day [args0] must be a number between 1 and {Days.Count}");
+                return;
+            }
+            Console.WriteLine("aoc21 <day> [input-file]");
+        }
+
+    }
+
+}
---
+++ day1/Day1.cs
@@ -0,0 +1,35 @@
+﻿using aoc.structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc21.day1
+{
+    internal class Day1 : Day
+    {
+
+        public Day1() : base(@"inputFiles\Day1Final.txt") { }
+
+        protected override string Compute(string[] commandLines)
+        {
+            int previousDepth = -1;
+            int increases = 0;
+            foreach(string commandLine in commandLines)
+            {
+                int currentDepth;
+                if (!int.TryParse(commandLine, out currentDepth))
+                    continue;
+
+                if(currentDepth > previousDepth)
+                    increases++;
+                previousDepth = currentDepth;
+            }
+            increases--;
+            return $"There are {increases} larger measurements in these values";
+        }
+
+
+    }
+}
